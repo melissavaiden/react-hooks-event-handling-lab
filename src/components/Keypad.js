6a32c53ccea2bead1b20b4491d3ff81ce18a2724
Melissa Vaiden <melissavaiden@gmail.com>
2022-06-20 17:51:05 -0400

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    return (
+        <input onChange={(e) => {
+            console.log('Entering password...')}} type="password" />
+    )
+}
+
+export default Keypad;
